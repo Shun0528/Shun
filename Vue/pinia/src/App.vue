06f818616b03384189eaef192dceea4560431e14
Shun <shun@macbook.local>
2023-09-24 22:50:50 +0900

    add pinia on Vue

---
+++ Vue/pinia/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Cost from "./components/cost.vue";
+import useStore from "./store/index";
+
+const { api } = useStore();
+const title = ref("Hello World");
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <table class="date">
+      <tr>
+        <td v-for="data in api.data">{{ data.date }}</td>
+      </tr>
+    </table>
+    <Cost />
+  </div>
+</template>
+
+<style scoped>
+.date {
+  border: 1px solid #000;
+  margin-bottom: 20px;
+}
+</style>
---
+++ Vue/pinia/src/components/cost.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import useStore from "../store/index";
+
+const { api } = useStore();
+</script>
+
+<template>
+    <div>
+        <p v-for="data in api.data">{{ data.cost }}</p>
+    </div>
+</template>
